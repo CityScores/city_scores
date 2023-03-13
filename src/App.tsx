4cd4f430218d1c08dbdb7303e3fffd2461bf5448
Misaki <tshimizumisaki@gmail.com>
2023-03-13 16:22:41 -0400

    Add mobile menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './styles/sass/styles.css';
-import Navigation from './components/Navigation/Navigation';
+import React from "react";
+import "./styles/sass/styles.css";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <header>City Scores</header>
-
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, qui
+        distinctio incidunt architecto hic, quis quod quos quam error temporibus
+        saepe voluptate exercitationem veritatis culpa mollitia! Ducimus,
+        tempore? Eius, fuga?
+      </p>
     </div>
   );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,39 +1,88 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-let name = "Parker"
+let name = "Parker";
 
 export default function Navigation() {
-    const [langButton, setLangButton] = useState(false)
+  const [mobileMenu, setMobileMenu] = useState(false);
 
-    const greeting = (name:string) => {
-        if(name){
-            return <span>Welcome, {name}</span>
-        }
-        else {
-            return <span>Welcome</span>
-        }
+  const greeting = (name: string) => {
+    if (name) {
+      return <span>Welcome, {name}</span>;
+    } else {
+      return <span>Welcome</span>;
     }
-    const handleLanguage = () => {
-        setLangButton(!langButton)
-    }
-    
-    
-    
-    return (
-        <nav >
-            <ul className="nav_tabs">
-                <li><a href="#"><h4>City Scores</h4></a></li>
-                <li><a href="#">Places</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Recent Searches</a></li>
-            </ul>
-            <ul className="nav_welcome">
-                <li>{greeting(name)}</li>
-                <li><button className="language_button" onClick={handleLanguage}>Choose your Language</button> {langButton ? (<ul>
-                    <li><button> English </button></li>
-                </ul>) : null}</li>
-            </ul>
-        </nav>
-    )
+  };
+  const handleMobileMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
+  return (
+    <nav>
+      <div className="mobileBttnContainer">
+        <button className="mobileMenuBttn" onClick={handleMobileMenu}>
+          <span className="material-symbols-outlined mobileMenuIcon">menu</span>
+        </button>
+      </div>
+      {mobileMenu ? (
+        <div className="mobileMenu">
+          <ul className="nav_tabs">
+            <li>
+              <a href="#">
+                <h4>City Scores</h4>
+              </a>
+            </li>
+            <li>{greeting(name)}</li>
+            <li>
+              <a href="#">Places</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Recent Searches</a>
+            </li>
+          </ul>
+          <ul className="nav_welcome">
+            <div className="language">
+              <li>
+                <button>EN</button>
+              </li>
+              <li>
+                <button>FR</button>
+              </li>
+            </div>
+          </ul>
+        </div>
+      ) : null}
+      <div className="deskMenu">
+        <ul className="nav_tabs">
+          <li>
+            <a href="#">
+              <h4>City Scores</h4>
+            </a>
+          </li>
+          <li>
+            <a href="#">Places</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Recent Searches</a>
+          </li>
+        </ul>
+        <ul className="nav_welcome">
+          <li>{greeting(name)}</li>
+          <div className="language">
+            <li>
+              <button>EN</button>
+            </li>
+            <li>
+              <button>FR</button>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </nav>
+  );
 }
- 
